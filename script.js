a35bc8c8c6325e61a72d6b359cd396a23cba09b5
MargaretMugo <71629260+MargaretMugo@users.noreply.github.com>
2022-10-26 09:46:38 +0300

    Assignment task

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 window.addEventListener('load', () => {
-    const form = document.querySelector(
-        "#new-task-form");
+    const form = document.querySelector("#new-task-form");
     const input = document.querySelector('#new-task-input');
-    const list_el = document.querySelector('#tasks')
+    const list_el = document.querySelector('#tasks');
+
     form.addEventListener('submit', (e) => {
+        const task_name = document.getElementById("new-task-input");
+        const valuee = task_name.value.trim();
+        const ol = list_el.querySelector('ol');
+        const li = document.createElement('li');
+
+        // Prevent page from reloading when submit button is clicked
         e.preventDefault();
 
+        // create a variable and assign the value of task input
         const task = input.value;
+        // create a div eleemnt and assin it to variable task_el
         const task_el = document.createElement('div');
+        // assign the class 'task' to the eleemnt created
         task_el.classList.add('task');
 
+        // same workflow as above
         const task_content_el = document.createElement('div');
         task_content_el.classList.add('content');
 
+        // make the task_conent_el a child of the task_el (div)
         task_el.appendChild(task_content_el);
 
+        // create an input field to store the task values
         const task_input_el = document.createElement('input');
-        task_input_el.classList.add('test');
+        task_input_el.classList.add('text');
         task_input_el.type = 'text';
+        // assign the task to the input field created above
         task_input_el.value = task;
 
         task_input_el.setAttribute("readonly", "readonly");
@@ -30,16 +43,24 @@ window.addEventListener('load', () => {
         task_edit_el.classList.add('edit');
         task_edit_el.innerText = 'Edit';
 
-        const task_delete_el = document.createElement('button');
-        task_edit_el.classList.add('Delete');
-        task_edit_el.innerText = 'Delete';
+        const task_del_el = document.createElement('button');
+        task_del_el.classList.add('delete');
+        task_del_el.innerText = 'Delete';
 
         task_action_el.appendChild(task_edit_el);
-        task_action_el.appendChild(task_delete_el);
+        task_action_el.appendChild(task_del_el);
 
         task_el.appendChild(task_action_el);
 
         input.value = '';
+        if (!valuee) {
+            alert("Please Enter Some Task");
+        } else {
+            li.innerHTML = input.value;
+            ol.appendChild(li);
+            list_el.appendChild(task_el);
+            input.value = '';
+        }
 
         list_el.appendChild(task_el);
 
@@ -49,13 +70,12 @@ window.addEventListener('load', () => {
                 task_input_el.removeAttribute("readonly");
                 task_input_el.focus();
             } else {
-                task_edit_el.innerText = "Edit"
+                task_edit_el.innerText = "Edit";
                 task_input_el.setAttribute("readonly", "readonly");
             }
         });
-        task_delete_el.addEventListener('click', (e) => {
+        task_del_el.addEventListener('click', (e) => {
             list_el.removeChild(task_el);
         });
-
     });
 });
